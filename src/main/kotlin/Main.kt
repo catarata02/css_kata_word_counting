ddf83fe0aa347af5fdadebd63fc4e1f65ad12384
Armin Häberling <armin.aha@gmail.com>
2024-06-05 13:39:12 +0200

    Task 3: Main fun

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,17 @@
 package org.example
 
-fun main() {
-    print("Enter text: ")
-    val text = readln()
-    val count = WordCounter().countWordsFromString(text)
+import java.io.File
+
+fun main(args: Array<String>) {
+    val wordCounter = WordCounter()
+    val count: Int
+    if (args.isEmpty()) {
+        print("Enter text: ")
+        val text = readln()
+        count = wordCounter.countWordsFromString(text)
+    } else {
+        count = wordCounter.countWordsFromFile(File(args[0].trim()))
+    }
     print("Number of words: $count")
     println()
 }
