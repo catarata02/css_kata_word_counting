4beec97532acc8882b26c2cf36b64b4c5efbbbef
catarata02 <catarata02@hotmail.com>
2024-06-05 14:29:38 +0200

    Task6: add average word length to console output

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,14 +4,14 @@ import java.io.File
 
 fun main(args: Array<String>) {
     val wordCounter = WordCounter()
-    val count: WordCountResult
+    val result: WordCountResult
     if (args.isEmpty()) {
         print("Enter text: ")
         val text = readln()
-        count = wordCounter.countWordsFromString(text)
+        result = wordCounter.countWordsFromString(text)
     } else {
-        count = wordCounter.countWordsFromFile(File(args[0].trim()))
+        result = wordCounter.countWordsFromFile(File(args[0].trim()))
     }
-    print("Number of words: ${count.words}, unique: ${count.uniqueWords}")
+    print("Number of words: ${result.words}, unique: ${result.uniqueWords}, average word length: ${result.averageWordLength}")
     println()
 }
